39fd0d9e1d4cba185781857325c3a70175e04077
Miguel Mota <hello@miguelmota.com>
2021-07-03 23:14:20 -0700

    Update example

--- example/main.js
+++ example/main.js
@@ -1,5 +1,7 @@
+// elements
 var $form = document.getElementById('form')
 var $input = document.getElementById('input')
+var $options = document.querySelectorAll('input[name="option"]')
 var $fillDefaultHashView = document.getElementById('fillDefaultHashView')
 var $fillDefaultHash = document.getElementById('fillDefaultHashValue')
 var $addressZero = document.getElementById('addressZero')
@@ -9,21 +11,20 @@ var $leaves = document.getElementById('leaves')
 var $layers = document.getElementById('layers')
 var $flatLayers = document.getElementById('flatLayers')
 var $tree = document.getElementById('tree')
+var $leaveSelect = document.getElementById('leaveSelect')
+var $proof = document.getElementById('proof')
+var $verifyForm = document.getElementById('verifyForm')
+var $verifyProof = document.getElementById('verifyProof')
+var $verifyLeaf = document.getElementById('verifyLeaf')
+var $verifyRoot = document.getElementById('verifyRoot')
+var $verified = document.getElementById('verified')
+
+// variables
 
 const addressZero = '0x0000000000000000000000000000000000000000000000000000000000000000'
 var hashFns = {
   sha256: window.sha256,
   keccak256: window.keccak256
-}
-
-function parseInput (value) {
-  value = $input.value.trim()
-  value = value.replace(/'/gi, '"')
-  try {
-    return JSON.parse(value)
-  } catch (err) {
-    return value.split('\n').filter(function (x) { return x })
-  }
 }
 
 var options = {
@@ -34,18 +35,22 @@ var options = {
   isBitcoinTree: false
 }
 
+var tree
+
+// functions
+
 function compute () {
   const value = getInputValue()
   const leaves = parseInput(value)
   const hash = getHashFn()
-  const fillDefaultHash = getDefaultFillHashValue()
+  const fillDefaultHash = getDefaultFillHashInput()
   const _options = Object.assign({}, options, {
     fillDefaultHash: options.fillDefaultHash ? fillDefaultHash : undefined
   })
   console.log('input leaves:', leaves)
   console.log('hash:', getHashType())
   console.log('options:', _options)
-  const tree = new window.MerkleTree(leaves, hash, _options)
+  tree = new window.MerkleTree(leaves, hash, _options)
   const hexRoot = tree.getHexRoot()
   const hexLeaves = tree.getHexLeaves()
   const hexLayers = tree.getHexLayers()
@@ -59,12 +64,96 @@ function compute () {
   setLayersValue(JSON.stringify(hexLayers, null, 2))
   setFlatLayersValue(JSON.stringify(hexFlatLayers, null, 2))
   setTreeValue(tree.toString())
+  updateLeaveOptions(hexLeaves)
+  updateProof(0)
+  setVerified('')
 }
+
+function parseInput (value) {
+  value = value.trim().replace(/'/gi, '"')
+  try {
+    return JSON.parse(value)
+  } catch (err) {
+    return value.split('\n')
+      .map(function (line) { return line.trim() })
+      .filter(function (line) { return line })
+  }
+}
+
+function updateLeaveOptions (leaves) {
+  $leaveSelect.innerHTML = ''
+  leaves.forEach(function (leaf, i) {
+    const el = document.createElement('option')
+    el.value = `${i}`
+    el.text = `#${i} - ${leaf}`
+    $leaveSelect.appendChild(el)
+  })
+}
+
+function updateProof (index) {
+  setProof('')
+  if (!tree) {
+    return
+  }
+  const leaves = tree.getHexLeaves()
+  if (!leaves.length) {
+    return
+  }
+  const leaf = leaves[index]
+  const proof = tree.getHexProof(leaf)
+  console.log('proof:', proof)
+  setProof(JSON.stringify(proof, null, 2))
+}
+
+function setVerified (verified) {
+  $verified.textContent = verified
+}
+
+function verify () {
+  setVerified('')
+  if (!tree) {
+    return
+  }
+  const proof = getVerifyProof()
+  const leaf = getVerifyLeaf()
+  const root = getVerifyRoot()
+  const verified = tree.verify(proof, leaf, root)
+  setVerified(`${verified}`)
+}
+
+// getters
 
 function getHashType () {
   const $hash = document.querySelector('input[name="hash"]:checked')
-  return $hash.value
+  return $hash.value.trim()
 }
+
+function getHashFn () {
+  const key = getHashType()
+  return hashFns[key]
+}
+
+function getDefaultFillHashInput () {
+  return $fillDefaultHash.value.trim()
+}
+
+function getInputValue (value) {
+  return $input.value.trim()
+}
+
+function getVerifyProof () {
+  return parseInput($verifyProof.value.trim())
+}
+
+function getVerifyLeaf () {
+  return $verifyLeaf.value.trim()
+}
+
+function getVerifyRoot () {
+  return $verifyRoot.value.trim()
+}
+
+// setters
 
 function setHashType (value) {
   if (!value) {
@@ -77,21 +166,10 @@ function setHashType (value) {
   $hash.checked = true
 }
 
-function getHashFn () {
-  const key = getHashType()
-  return hashFns[key]
-}
-
-function getDefaultFillHashValue () {
-  return $fillDefaultHash.value
-}
-
-function getInputValue (value) {
-  return $input.value
-}
-
-function setInputValue (value) {
-  $input.value = value
+function setInputValue (value, onlySave) {
+  if (!onlySave) {
+    $input.value = value
+  }
   try {
     localStorage.setItem('input', value)
   } catch (err) {
@@ -100,19 +178,19 @@ function setInputValue (value) {
 }
 
 function setRootValue (value) {
-  $root.value = value
+  $root.textContent = value
 }
 
 function setLeavesValue (value) {
-  $leaves.value = value
+  $leaves.textContent = value
 }
 
 function setLayersValue (value) {
-  $layers.value = value
+  $layers.textContent = value
 }
 
 function setFlatLayersValue (value) {
-  $flatLayers.value = value
+  $flatLayers.textContent = value
 }
 
 function setTreeValue (value) {
@@ -127,8 +205,14 @@ function setHashValue (value) {
   }
 }
 
-function setOptionValue (key, enabled) {
+function setOptionValue (key, enabled, onlySave) {
   try {
+    if (!onlySave) {
+      var $option = document.querySelector(`input[name="option"][id="${key}"]`)
+      if ($option) {
+        $option.checked = enabled
+      }
+    }
     options[key] = enabled
     localStorage.setItem('options', JSON.stringify(options))
     toggleFillDefaultHashView()
@@ -137,13 +221,49 @@ function setOptionValue (key, enabled) {
   }
 }
 
-function setFillDefaultHash (value) {
-  $fillDefaultHash.value = value
-}
-
-function setDefaultFillHashValue (value) {
+function setFillDefaultHash (value, onlySave) {
+  if (!onlySave) {
+    $fillDefaultHash.value = value
+  }
   try {
     localStorage.setItem('fillDefaultHash', value)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+function setProof (value) {
+  $proof.textContent = value
+}
+
+function setVerifyProof (value, onlySave) {
+  if (!onlySave) {
+    $verifyProof.value = value
+  }
+  try {
+    localStorage.setItem('verifyProof', value)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+function setVerifyLeaf (value, onlySave) {
+  if (!onlySave) {
+    $verifyLeaf.value = value
+  }
+  try {
+    localStorage.setItem('verifyLeaf', value)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+function setVerifyRoot (value, onlySave) {
+  if (!onlySave) {
+    $verifyRoot.value = value
+  }
+  try {
+    localStorage.setItem('verifyRoot', value)
   } catch (err) {
     console.error(err)
   }
@@ -157,35 +277,41 @@ function toggleFillDefaultHashView () {
   }
 }
 
+// event listeners
+
 $form.addEventListener('submit', function (event) {
   event.preventDefault()
   compute()
 })
 
+$verifyForm.addEventListener('submit', function (event) {
+  event.preventDefault()
+  verify()
+})
+
 $input.addEventListener('input', function (event) {
-  const value = event.target.value
-  setInputValue(value)
+  const value = event.target.value.trim()
+  setInputValue(value, true)
 })
 
 var $hashes = document.querySelectorAll('input[name="hash"]')
 $hashes.forEach(function ($hash) {
   $hash.addEventListener('change', function (event) {
-    const value = event.target.value
+    const value = event.target.value.trim()
     setHashValue(value)
   })
 })
 
-var $options = document.querySelectorAll('input[name="option"]')
 $options.forEach(function ($option) {
   $option.addEventListener('change', function (event) {
-    const value = event.target.value
+    const value = event.target.value.trim()
     const checked = event.target.checked
-    setOptionValue(value, checked)
+    setOptionValue(value, checked, true)
   })
 })
 
 $fillDefaultHash.addEventListener('input', function (event) {
-  const value = event.target.value
+  const value = event.target.value.trim()
   setFillDefaultHash(value)
 })
 
@@ -199,6 +325,27 @@ $hashAddressZero.addEventListener('click', function (event) {
   const result = '0x' + hash(addressZero).toString('hex')
   setFillDefaultHash(result)
 })
+
+$leaveSelect.addEventListener('change', function (event) {
+  updateProof(Number(event.target.value.trim()))
+})
+
+$verifyProof.addEventListener('input', function (event) {
+  const value = event.target.value.trim()
+  setVerifyProof(value, true)
+})
+
+$verifyLeaf.addEventListener('input', function (event) {
+  const value = event.target.value.trim()
+  setVerifyLeaf(value, true)
+})
+
+$verifyRoot.addEventListener('input', function (event) {
+  const value = event.target.value.trim()
+  setVerifyRoot(value, true)
+})
+
+// init
 
 function load () {
   try {
@@ -221,6 +368,9 @@ function load () {
     const value = JSON.parse(localStorage.getItem('options'))
     if (value) {
       options = value
+      for (const option in options) {
+        setOptionValue(option, options[option])
+      }
     }
   } catch (err) {
     console.error(err)
@@ -228,14 +378,41 @@ function load () {
   try {
     const value = localStorage.getItem('fillDefaultHash')
     if (value) {
-      setDefaultFillHashValue(value)
+      setFillDefaultHash(value)
     }
   } catch (err) {
     console.error(err)
   }
+  try {
+    const value = localStorage.getItem('verifyProof')
+    if (value) {
+      setVerifyProof(value)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  try {
+    const value = localStorage.getItem('verifyLeaf')
+    if (value) {
+      setVerifyLeaf(value)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  try {
+    const value = localStorage.getItem('verifyRoot')
+    if (value) {
+      setVerifyRoot(value)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
 
+function main () {
+  load()
   toggleFillDefaultHashView()
   compute()
 }
 
-load()
+main()
